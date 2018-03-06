228839604e45944756f068e0103869e3af1fd2c6
Brian Hanus <brian.hanus@ecolab.com>
2018-03-05 21:14:19 -0600

    Initial Commit

---
+++ LatentDevelopment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LatentDevelopment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
